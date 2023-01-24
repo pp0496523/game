8c92ae026f76665d7c3c794e66f739a5ecc05307
pp0496523 <pp0496523@gmail.com>
2023-01-24 23:27:03 +0900

    左右移動実装

--- Assets/Scripts/View/PlayerView.cs
+++ Assets/Scripts/View/PlayerView.cs
@@ -10,16 +10,25 @@ namespace View
     {
 
         Rigidbody2D _rb;
+        Animator _animator;
+        private SpriteRenderer _spriteRenderer;
 
         void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         public void Move(Vector2 vector2, float moveSpeed)
         {
-            float _xSpeed = Math.Sign(vector2.x) * moveSpeed;
+            float x = vector2.x;
+            float _xSpeed = Math.Sign(x) * moveSpeed;
             _rb.velocity = new Vector2(_xSpeed, _rb.velocity.y);
+            _animator.SetFloat("speed", Math.Abs(x));
+
+            if (x > 0) _spriteRenderer.flipX = false;
+            else if (x < 0) _spriteRenderer.flipX = true;
         }
     }
 }
